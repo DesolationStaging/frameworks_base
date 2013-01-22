63f388f28f061a2a89f5d0f44bc67c2fb89f2ed0
Kalyani polepeddy <kpolep@codeaurora.org>
2013-01-22 14:35:18 +0530

    frameworks/base: Fix to avoid crash when the tab is not set.
    Tab is not highlighted in Videoapp unlike Music app.
    Instead the list is highlighted with mSelectedTab a negative value
    when onFocusChange is called.
    Add a check for mSelectedTab.
    
    Change-Id: I930657b8a9be4be4d973f876242a15a667ec6f94
    CRs-Fixed: 657606

--- core/java/android/widget/TabWidget.java
+++ core/java/android/widget/TabWidget.java
@@ -312,9 +312,11 @@ public class TabWidget extends LinearLayout implements OnFocusChangeListener {
 
     @Override
     public void childDrawableStateChanged(View child) {
-        if (getTabCount() > 0 && child == getChildTabViewAt(mSelectedTab)) {
-            // To make sure that the bottom strip is redrawn
-            invalidate();
+        if (mSelectedTab != -1) {
+            if (getTabCount() > 0 && child == getChildTabViewAt(mSelectedTab)) {
+                // To make sure that the bottom strip is redrawn
+                invalidate();
+            }
         }
         super.childDrawableStateChanged(child);
     }
@@ -390,7 +392,6 @@ public class TabWidget extends LinearLayout implements OnFocusChangeListener {
         if (index < 0 || index >= getTabCount() || index == mSelectedTab) {
             return;
         }
-
         if (mSelectedTab != -1) {
             getChildTabViewAt(mSelectedTab).setSelected(false);
         }
@@ -515,7 +516,7 @@ public class TabWidget extends LinearLayout implements OnFocusChangeListener {
 
     /** {@inheritDoc} */
     public void onFocusChange(View v, boolean hasFocus) {
-        if (v == this && hasFocus && getTabCount() > 0) {
+        if (v == this && hasFocus && getTabCount() > 0 && ( mSelectedTab!= -1) ) {
             getChildTabViewAt(mSelectedTab).requestFocus();
             return;
         }
