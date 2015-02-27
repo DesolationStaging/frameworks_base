234bdef2eb704ca085d546ca0e64c3a73fa5f693
maxwen <max.weninger@gmail.com>
2015-02-27 18:51:25 +0100

    base: simplify volume panel slide in
    lets do it the 'android way'
    
    Change-Id: Icf2ff984efe5b8a0081c06fffda8a37d0c216950
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -1259,7 +1259,6 @@ public class VolumePanel extends Handler implements DemoMode {
     }
 
     protected void onPlaySound(int streamType, int flags) {
-
         // If preference is no sound - just exit here
         if (Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.VOLUME_ADJUST_SOUNDS_ENABLED, 1) == 0) {
