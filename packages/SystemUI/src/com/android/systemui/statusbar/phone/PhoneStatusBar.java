14766c2593f1c3442cf65bc0814aa1a6e2a5b2fd
ezio84 <brabus84@gmail.com>
2015-05-18 13:09:35 +0200

    Frameworks: SlimPie (1/2)
    Porting from slimkat 4.4:
    - https://github.com/SlimRoms/frameworks_base/commit/6e52c566418521888b305e1c5937ec348ad6b03a
    - https://github.com/SlimRoms/frameworks_base/commit/ebfa8576c936a232bc616729cfbb350bd83203f0
    - https://github.com/SlimRoms/frameworks_base/commit/8912615e2d0210c602c3e3768e0086f609e79f49
    - https://github.com/SlimRoms/frameworks_base/commit/4d566f2bfccf53f51c830a02fb1d663845c47cf2
    - adapted to lp5.1 source and the lp5.1 Griffin's custom navbar code
    
    - patchset: added pie related code from:
    https://github.com/SlimRoms/frameworks_base/commit/e08fd5a64863548fab312acffa683e1ec2e795df
    https://github.com/SlimRoms/frameworks_base/commit/c8a2ce3493d9d31c118961e79d6d19951fd82790
    https://github.com/SlimRoms/frameworks_base/commit/4097a175f00fa0137169c4ae4a6bc35a2799c97f
    
    PS6-7-8: fixed copyright date - fix format
    PS9: Rebase, conflict fix
    
    Change-Id: Id52c4490d4d9a48fd35ae77f3619bec074b38268
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/dimens.xml
    	core/res/res/values/slim_strings.xml
    	core/res/res/values/slim_symbols.xml
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/slim_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    	services/java/com/android/server/SystemServer.java
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -484,6 +484,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.MENU_VISIBILITY),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.PIE_CONTROLS), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -521,6 +524,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         mBatterySaverWarningColor = mContext.getResources()
                                 .getColor(com.android.internal.R.color.battery_saver_mode_color);
                     }
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.PIE_CONTROLS))) {
+                attachPieContainer(isPieEnabled());
             }
             super.onChange(selfChange, uri);
 
@@ -569,6 +575,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.STATUS_BAR_CARRIER, 0, mCurrentUserId) == 1;
             showStatusBarCarrierLabel(mShowStatusBarCarrier);
         }
+    }
+
+    private boolean isPieEnabled() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PIE_CONTROLS, 0,
+                UserHandle.USER_CURRENT) == 1;
     }
 
     // ensure quick settings is disabled until the current user makes it through the setup wizard
@@ -902,6 +914,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         updateShowSearchHoldoff();
 
+        // Setup pie container if enabled
+        attachPieContainer(isPieEnabled());
+
         if (mNavigationBarView == null) {
             mNavigationBarView =
                 (NavigationBarView) View.inflate(context, R.layout.navigation_bar, null);
@@ -909,6 +924,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mNavigationBarView.setDisabledFlags(mDisabled);
         mNavigationBarView.setBar(this);
+        addNavigationBarCallback(mNavigationBarView);
         mNavigationBarView.setOnVerticalChangedListener(
                 new NavigationBarView.OnVerticalChangedListener() {
             @Override
@@ -1445,6 +1461,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         prepareNavigationBarView();
 
         mWindowManager.addView(mNavigationBarView, getNavigationBarLayoutParams());
+        mNavigationBarOverlay.setNavigationBar(mNavigationBarView);
     }
 
     private void repositionNavigationBar() {
@@ -2359,8 +2376,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         | StatusBarManager.DISABLE_RECENT
                         | StatusBarManager.DISABLE_BACK
                         | StatusBarManager.DISABLE_SEARCH)) != 0) {
-            // the nav bar will take care of these
-            if (mNavigationBarView != null) mNavigationBarView.setDisabledFlags(state);
+
+            // All navigation bar listeners will take care of these
+            propagateDisabledFlags(state);
 
             if ((state & StatusBarManager.DISABLE_RECENT) != 0) {
                 // close recents if it's visible
@@ -2948,9 +2966,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mNavigationIconHints = hints;
 
-        if (mNavigationBarView != null) {
-            mNavigationBarView.setNavigationIconHints(hints);
-        }
+        propagateNavigationIconHints(hints);
         checkBarModes();
     }
 
@@ -3224,9 +3240,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (DEBUG) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
         }
-        if (mNavigationBarView != null) {
-            mNavigationBarView.setMenuVisibility(showMenu);
-        }
+
+        propagateMenuVisibility(showMenu);
 
         // See above re: lights-out policy for legacy apps.
         if (showMenu) setLightsOn(true);
