cristianomatos/android_frameworks_base

Base: HeadsUp options [1/2] Includes:
* Base: forward port Headsup options (1/2)

-Global on/off
-Blacklist and Do not disturb

(by Adnan Begovic)

* HeadsUp: fix settings not being applied after reboot

blacklist and dnd setting were not being applied at boot
clean up the SettingsObserver

(by Altaf Patel)

* My changes

- Let user choose if headsup swype action hides or dimisses notification

Change-Id: Idca8dd715b13838a8c59a31276bfe9445651d6f7

core/java/android/provider/Settings.java
@@ -3407,6 +3407,13 @@ public final class Settings {
          */
         public static final String STATUS_BAR_SHOW_WEATHER = "status_bar_show_weather";
 
+
+        /**
+        * Hide notification instead of remove when swiping headsup
+        * @hide
+        */
+        public static final String HEADS_UP_DISMISS_ON_REMOVE = "heads_up_swipe_action";
+
         /**
          * MediaScanner behavior on boot.
          * 0 = enabled
@@ -3682,6 +3689,27 @@ public final class Settings {
          * @hide
          */
         public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
+         * Heads Up Notifications
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_NOTIFICATION = "heads_up_enabled";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings

DesolationStaging/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
	core/java/android/provider/Settings.java
	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
	packages/SystemUI/res/values/config.xml

core/java/android/provider/Settings.java
@@ -2978,6 +2978,30 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
+         * Heads Up Notifications
+         * @hide
+         */
+        public static final String HEADS_UP_NOTIFICATION = "heads_up_enabled";
+
+        /**
+         * Which applications to disable heads up notifications for
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+
+        /**
+         * Which applications to disable heads up notifications for
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
+        /**
+         * Hide notification instead of remove when swiping headsup
+         * @hide
+         */
+        public static final String HEADS_UP_DISMISS_ON_REMOVE = "heads_up_dismiss_on_remove";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *